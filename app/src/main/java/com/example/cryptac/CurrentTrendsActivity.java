07eebdf20d2c2ab45c1398c60a3b28e52c39114e
sneha-tandri <shyamsneha25@gmail.com>
2022-04-27 14:06:10 +0530

    Home page added

--- app/src/main/java/com/example/cryptac/CurrentTrendsActivity.java
+++ app/src/main/java/com/example/cryptac/CurrentTrendsActivity.java
@@ -1,67 +1,68 @@
 package com.example.cryptac;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.ListView;
-import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.toptoche.searchablespinnerlibrary.SearchableSpinner;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
 import java.util.ArrayList;
-
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import java.util.Arrays;
+import java.util.List;
 
 public class CurrentTrendsActivity extends AppCompatActivity{
 
     private Button btn;
     private SearchableSpinner searchableSpinner1;
     private SearchableSpinner searchableSpinner2;
-    private ArrayList<String> coinSymbols = new ArrayList<String>();
-    private ArrayList<String> coins = new ArrayList<String>();
-    private ArrayList<String> currency = new ArrayList<String>();
+    private List<String> coinSymbols = new ArrayList<String>();
+    private List<String> coins = new ArrayList<String>();
+    private List<String> currencySymbols = new ArrayList<String>();
+    private List<String> currency = new ArrayList<String>();
     private ImageButton backButton;
     private String setCoinSym;
     private String setCoin;
     private String setCurrency;
+    private String setCurrencySym;
+    private String coinNamesStr;
+    private String coinSymbolsStr;
+    private String currSymStr;
+    private String currNamesStr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_current_trends);
+        coinNamesStr = getString(R.string.coin_names);
+        coinSymbolsStr = getString(R.string.coin_symbols);
+        currNamesStr = getString(R.string.currency);
+        currSymStr = getString(R.string.currency_symbols);
 
-        //spinner 1
-        coins.add("");
-        coinSymbols.add("Search Cryptocurrencies");
-        populateCoins();
-        while(coinSymbols.size() == 1){};
+        String str[] = coinSymbolsStr.split(",");
+        coinSymbols.addAll(Arrays.asList(str));
+        str = coinNamesStr.split(",");
+        coins.addAll(Arrays.asList(str));
+        str = currNamesStr.split(",");
+        currency.addAll(Arrays.asList(str));
+        str = currSymStr.split(",");
+        currencySymbols.addAll(Arrays.asList(str));
+
+        coins.add(0, "");
+        coinSymbols.add(0, "Search Cryptocurrencies");
+        currency.add(0, "");
+        currencySymbols.add(0, "Search currencies");
+
         searchableSpinner1 = findViewById(R.id.spinner1);
         ArrayAdapter<String> sp1ArrayAdapter = new ArrayAdapter<String>(
                 this,
                 android.R.layout.simple_spinner_dropdown_item,
                 coinSymbols);
         searchableSpinner1.setAdapter(sp1ArrayAdapter);
-        searchableSpinner1 = findViewById(R.id.spinner1);
         searchableSpinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
@@ -78,21 +79,17 @@ public class CurrentTrendsActivity extends AppCompatActivity{
         });
 
         //spinner 2
-        currency.add("Search currencies");
-        populateCurrency();
-        while(currency.size() == 1){};
         searchableSpinner2 = findViewById(R.id.spinner2);
-        searchableSpinner2.setTitle("Search Cryptocurrencies");
-        searchableSpinner2.setPositiveButton("OK");
         ArrayAdapter<String> sp2ArrayAdapter = new ArrayAdapter<String>(
                 this,
                 android.R.layout.simple_spinner_dropdown_item,
-                currency);
+                currencySymbols);
         searchableSpinner2.setAdapter(sp2ArrayAdapter);
         searchableSpinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int pos, long l) {
                 if(pos != 0){
+                    setCurrencySym = currencySymbols.get(pos);
                     setCurrency = currency.get(pos);
                 }
             }
@@ -110,8 +107,9 @@ public class CurrentTrendsActivity extends AppCompatActivity{
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), RedirectionPage.class);
                 intent.putExtra("coin",setCoin);
-                intent.putExtra("currency", setCurrency);
+                intent.putExtra("currencySym", setCurrencySym);
                 intent.putExtra("coinSymbol",setCoinSym);
+                intent.putExtra("currency",setCurrency);
                 startActivity(intent);
             }
         });
@@ -127,75 +125,4 @@ public class CurrentTrendsActivity extends AppCompatActivity{
 
     }
 
-    public void populateCoins(){
-        OkHttpClient client = new OkHttpClient().newBuilder()
-                .build();
-        Request request = new Request.Builder()
-                .url("https://api.coincap.io/v2/assets")
-                .build();
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(@NonNull Call call, @NonNull IOException e) {
-                e.printStackTrace();
-            }
-
-            @Override
-            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-                if (response.isSuccessful()) {
-
-                    final String myResponse = response.body().string();
-                    JSONObject obj = null;
-                    try {
-                        obj = new JSONObject(myResponse);
-                        JSONArray arr = obj.getJSONArray("data");
-                        int i;
-                        for(i = 0; i<arr.length(); i++){
-                            String coin_sym = arr.getJSONObject(i).getString("symbol");
-                            String coin = arr.getJSONObject(i).getString("id");
-                            coins.add(coin);
-                            coinSymbols.add(coin_sym);
-                        }
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-
-                }
-            }
-        });
-    }
-
-    public void populateCurrency(){
-        OkHttpClient client = new OkHttpClient().newBuilder()
-                .build();
-        Request request = new Request.Builder()
-                .url("https://api.coincap.io/v2/rates")
-                .build();
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(@NonNull Call call, @NonNull IOException e) {
-                e.printStackTrace();
-            }
-
-            @Override
-            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-                if (response.isSuccessful()) {
-
-                    final String myResponse = response.body().string();
-                    JSONObject obj = null;
-                    try {
-                        obj = new JSONObject(myResponse);
-                        JSONArray arr = obj.getJSONArray("data");
-                        int i;
-                        for(i = 0; i<arr.length(); i++){
-                            String cur_sym = arr.getJSONObject(i).getString("symbol");
-                            currency.add(cur_sym);
-                        }
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-
-                }
-            }
-        });
-    }
 }
--- app/src/main/java/com/example/cryptac/MainActivity.java
+++ app/src/main/java/com/example/cryptac/MainActivity.java
@@ -2,36 +2,57 @@ package com.example.cryptac;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.LinearGradient;
-import android.graphics.Shader;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.squareup.picasso.Picasso;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+class item{
+    String ID;
+    String sym;
+    Double percent;
+    Double value;
+    item(String ID, String sym, Double percent, Double value){
+        this.ID = ID;
+        this.sym = sym;
+        this.percent = percent;
+        this.value = value;
+    }
+}
 
 public class MainActivity extends AppCompatActivity {
-
+    item topGain, topLoss;
+    ArrayList<item> a = new ArrayList<>(); //arraylist of all items
+    String url1 = "https://www.cryptologos.cc/logos/";
+    String url2 = "-logo.png";
     BottomNavigationView bottom_navigation;
 
-    private Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn = findViewById(R.id.btn);
+        Button btn = findViewById(R.id.btn);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -39,6 +60,79 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        populateArray();
+        while(a.size() < 5);
+        String[] url3 = new String[5];
+
+        for(int i=0; i<5; i++){
+            String str = a.get(i).ID+"-" + a.get(i).sym.toLowerCase();
+            url3[i] = str;
+        }
+        topGain = a.get(0);
+        topLoss = a.get(0);
+        while(a.size()!=100);
+        for(int i=0; i<a.size(); i++){
+
+            if(a.get(i).percent > topGain.percent){
+                topGain = a.get(i);
+            }
+            if(a.get(i).percent < topLoss.percent){
+                topLoss = a.get(i);
+            }
+        }
+
+        String str = topGain.ID+"-" + topGain.sym.toLowerCase();
+        TextView coinNA = findViewById(R.id.coinNameA);
+        TextView coinVA = findViewById(R.id.coinValueA);
+        ImageView imageView = findViewById(R.id.imageViewA);
+        Picasso.get().load(url1+str+url2).placeholder(R.drawable.ic_baseline_image_24).resize(200, 200)
+                .centerCrop().into(imageView);
+        coinNA.setText(topGain.sym); coinVA.setText("$" + topGain.value);
+
+        str = topLoss.ID+"-" + topLoss.sym.toLowerCase();
+        TextView coinNB = findViewById(R.id.coinNameB);
+        TextView coinVB = findViewById(R.id.coinValueB);
+        imageView = findViewById(R.id.imageViewB);
+        Picasso.get().load(url1+str+url2).placeholder(R.drawable.ic_baseline_image_24).resize(200, 200)
+                .centerCrop().into(imageView);
+        coinNB.setText(topLoss.sym); coinVB.setText("$" + topLoss.value);
+
+        TextView coinN1 = findViewById(R.id.coinName1);
+        TextView coinV1 = findViewById(R.id.coinValue1);
+        imageView = findViewById(R.id.imageView1);
+        Picasso.get().load(url1+url3[0]+url2).placeholder(R.drawable.ic_baseline_image_24).resize(170, 170)
+                .centerCrop().into(imageView);
+        coinN1.setText(a.get(0).sym); coinV1.setText("$" + a.get(0).value);
+
+        TextView coinN2 = findViewById(R.id.coinName2);
+        TextView coinV2 = findViewById(R.id.coinValue2);
+        imageView = findViewById(R.id.imageView2);
+        Picasso.get().load(url1+url3[1]+url2).placeholder(R.drawable.ic_baseline_image_24).resize(170, 170)
+                .centerCrop().into(imageView);
+        coinN2.setText(a.get(1).sym); coinV2.setText("$" + a.get(1).value);
+
+
+        TextView coinN3 = findViewById(R.id.coinName3);
+        TextView coinV3 = findViewById(R.id.coinValue3);
+        imageView = findViewById(R.id.imageView3);
+        Picasso.get().load(url1+url3[2]+url2).placeholder(R.drawable.ic_baseline_image_24).resize(170, 170)
+                .centerCrop().into(imageView);
+        coinN3.setText(a.get(2).sym); coinV3.setText("$" + a.get(2).value);
+
+        TextView coinN4 = findViewById(R.id.coinName4);
+        TextView coinV4 = findViewById(R.id.coinValue4);
+        imageView = findViewById(R.id.imageView4);
+        Picasso.get().load(url1+url3[3]+url2).placeholder(R.drawable.ic_baseline_image_24).resize(170, 170)
+                .centerCrop().into(imageView);
+        coinN4.setText(a.get(3).sym); coinV4.setText("$" + a.get(3).value);
+
+        TextView coinN5 = findViewById(R.id.coinName5);
+        TextView coinV5 = findViewById(R.id.coinValue5);
+        imageView = findViewById(R.id.imageView5);
+        Picasso.get().load(url1+url3[4]+url2).placeholder(R.drawable.ic_baseline_image_24).resize(170, 170)
+                .centerCrop().into(imageView);
+        coinN5.setText(a.get(4).sym); coinV5.setText("$" + a.get(4).value);
 
         bottom_navigation = findViewById(R.id.bottom_navigation);
         bottom_navigation.setSelectedItemId(R.id.page_2);
@@ -49,12 +143,14 @@ public class MainActivity extends AppCompatActivity {
                     case R.id.page_1:
                         startActivity(new Intent(getApplicationContext(), CalculateActivity.class));
                         overridePendingTransition(0,0);
+                        finish();
                         return true;
                     case R.id.page_2:
                         return true;
                     case R.id.page_3:
                         startActivity(new Intent(getApplicationContext(), SettingsActivity.class));
                         overridePendingTransition(0,0);
+                        finish();
                         return true;
                 }
                 return false;
@@ -62,4 +158,45 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+    void populateArray(){
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        Request request = new Request.Builder()
+                .url("https://api.coincap.io/v2/assets")
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if (response.isSuccessful()) {
+
+                    final String myResponse = response.body().string();
+                    JSONObject obj;
+                    try {
+                        obj = new JSONObject(myResponse);
+                        JSONArray arr = obj.getJSONArray("data");
+                        while(arr.length() == 0);
+                        for(int i = 0;i < arr.length();i++){
+                            Double val = Double.parseDouble(arr.getJSONObject(i).getString("priceUsd"));
+                            double val2 = (double) Math.round(val * 100) / 100;
+                            item it = new item(arr.getJSONObject(i).getString("id"),
+                                    arr.getJSONObject(i).getString("symbol"),
+                                    Double.parseDouble(arr.getJSONObject(i).getString("changePercent24Hr")),
+                                    val2);
+                            a.add(it);
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        });
+    }
+
 }
